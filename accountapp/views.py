84b619d1b41c8f98311ea908e66bc84e336582a6
seonjin <yellow3978@gmail.com>
2021-07-13 09:26:37 +0900

    7/13 my_project commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,12 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import NewModel
 
 
 def my_project(request):
     if request.method == "POST" :
+
+        temp = request.POST.get('input_text')
+
+        model_instance = NewModel()       # 저장하는 과정
+        model_instance.text = temp
+        model_instance.save()
+
+
         return render(request, 'accountapp/my_project.html',
-                      context={'text': 'POST METHOD!'})
+                      context={'model_instance': model_instance})
     else :
         return render(request, 'accountapp/my_project.html',
                       context={'text': 'POST METHOD!'})
