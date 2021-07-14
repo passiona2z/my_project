b60c71d88cb1bd2842e1cc3f56fc42c04e45710a
seonjin <yellow3978@gmail.com>
2021-07-14 16:16:59 +0900

    7/14 my_project commit end

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
 from accountapp.views import my_project, AccountCreateView
@@ -6,6 +7,10 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('my_project/', my_project, name='my_project'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create')
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -30,5 +30,5 @@ def my_project(request):
 class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp: hello_world')
+    success_url = reverse_lazy('accountapp:my_project')
     template_name = 'accountapp/create.html'
