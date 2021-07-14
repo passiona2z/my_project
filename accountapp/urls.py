e0818bea02680f7f7bc8cc96928d7932766c480c
seonjin <yellow3978@gmail.com>
2021-07-14 14:54:05 +0900

    7/14 my_project commit 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from accountapp.views import my_project
+from accountapp.views import my_project, AccountCreateView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('my_project/', my_project, name='my_project')
+    path('my_project/', my_project, name='my_project'),
+
+    path('create/', AccountCreateView.as_view(), name='create')
+
 ]
